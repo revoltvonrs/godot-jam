ad67469622b3d8793d634d4a5572a2974ccf187e
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 16:48:14 +0200

    HUD update

--- Objects/Clown/Clown.gd
+++ Objects/Clown/Clown.gd
@@ -1,15 +1,13 @@
 extends KinematicBody
 
 var speed = 10
+var state = 3 # [3, 2, 1, 0]
 
 onready var player = get_parent().get_node("../Player")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	$Sprite3D.look_at(Vector3(player.global_transform.origin.x,global_transform.origin.y, player.global_transform.origin.z),Vector3(0,1,0))
 	var player_position = player.global_transform.origin
--- Objects/Gifts.gd
+++ Objects/Gifts.gd
@@ -6,7 +6,5 @@ const GiftsEnum = {
 }
 const Gifts = [GiftsEnum.Banana, GiftsEnum.Soap]
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
