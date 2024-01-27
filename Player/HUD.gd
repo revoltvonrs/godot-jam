a6cc026bb6179f10b26c1c61c64d4287f97e7bb1
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 17:15:58 +0200

    Art and HUD

--- Player/HUD.gd
+++ Player/HUD.gd
@@ -1,6 +1,21 @@
 extends Control
 
+var previousState = 3
 var gift = null
+
+onready var clown = get_node("/root/TestK/Clown")
+
+#func grayscale():
+
+func highlight(state: int):
+	pass
+	#match state:
+		#3:
+			
+
+func _process(delta):
+	if clown.state != previousState:
+		highlight(clown.state)
 
 func _ready():
 	pass
