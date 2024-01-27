7089e98c02b6b59113ac352fea404748c74cda18
novomiracle <novomiracle@gmail.com>
2024-01-27 17:09:31 +0200

    banana

--- Objects/Clown/Clown.gd
+++ Objects/Clown/Clown.gd
@@ -2,17 +2,32 @@ extends KinematicBody
 
 var speed = 10
 var state = 3 # [3, 2, 1, 0]
-
+var stun = false
 onready var player = get_parent().get_node("../Player")
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	$Sprite3D.look_at(Vector3(player.global_transform.origin.x,global_transform.origin.y, player.global_transform.origin.z),Vector3(0,1,0))
-	var player_position = player.global_transform.origin
-	var direction = (player_position - global_transform.origin).normalized()
+	if !stun:
+		$Sprite3D.look_at(Vector3(player.global_transform.origin.x,global_transform.origin.y, player.global_transform.origin.z),Vector3(0,1,0))
+		var player_position = player.global_transform.origin
+		var direction = (player_position - global_transform.origin).normalized()
+		move_and_slide(direction * 5)
 	#direction.y = 0
 	#print(direction.y)
-	move_and_slide(direction * 5)
 	
+
+
+
+func _on_Area_area_entered(area):
+	match area.name:
+		"Banana":
+			print("animation slip")
+			stun = true
+			yield(get_tree().create_timer(3), "timeout")
+			stun = false
+			print("backUp")
+	area.get_parent().queue_free()
+
+
