a8ff8bd72b2579fa7da8a576c595f7649f697616
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 13:11:45 +0200

    Clown movement

--- Objects/Clown/Clown.gd
+++ Objects/Clown/Clown.gd
@@ -1,15 +1,14 @@
 extends "res://Objects/object.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var player_position = Player.global_transform.origin
+	var direction = (player_position - global_transform.origin).normalized()
+	direction.y = 0
+	move_and_slide(direction * 10)
+	
