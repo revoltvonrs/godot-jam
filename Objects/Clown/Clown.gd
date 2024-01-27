1b0aa72876ae82fae54cc82062ddbb4f440a4c8a
novomiracle <novomiracle@gmail.com>
2024-01-27 17:39:04 +0200

    balloon

--- Objects/Clown/Clown.gd
+++ Objects/Clown/Clown.gd
@@ -1,10 +1,10 @@
 extends KinematicBody
 
-var speed = 10
+var speed = 5
 var state = 3 # [3, 2, 1, 0]
 var stun = false
 onready var player = get_parent().get_node("../Player")
-
+var direction 
 func _ready():
 	pass
 
@@ -12,8 +12,8 @@ func _physics_process(delta):
 	if !stun:
 		$Sprite3D.look_at(Vector3(player.global_transform.origin.x,global_transform.origin.y, player.global_transform.origin.z),Vector3(0,1,0))
 		var player_position = player.global_transform.origin
-		var direction = (player_position - global_transform.origin).normalized()
-		move_and_slide(direction * 5)
+		direction = (player_position - global_transform.origin).normalized()
+	move_and_slide(direction * speed)
 	#direction.y = 0
 	#print(direction.y)
 	
@@ -21,13 +21,24 @@ func _physics_process(delta):
 
 
 func _on_Area_area_entered(area):
+	var drop = true
 	match area.name:
 		"Banana":
 			print("animation slip")
 			stun = true
+			direction = Vector3()
 			yield(get_tree().create_timer(3), "timeout")
 			stun = false
 			print("backUp")
-	area.get_parent().queue_free()
+		"Balloon":
+			stun = true
+			direction = Vector3(0,1,0)
+			yield(get_tree().create_timer(2), "timeout")
+			stun = false
+		_:
+			drop = false
+	if drop:
+		area.get_parent().activate()
+		area.get_parent().queue_free()
 
 
