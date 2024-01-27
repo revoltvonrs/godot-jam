e04aeed8d1334283db05924b9205215f89d194e5
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 17:27:38 +0200

    HUD update

--- Levels/TestK/TestK.gd
+++ Levels/TestK/TestK.gd
@@ -27,12 +27,15 @@ func make_gift():
 	var gift = load("res://Objects/Gift/Gift.tscn").instance()
 	
 	var bounds = get_aabb_global_endpoints(mesh_instance)
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
 	
 	var random_x = rand_range(bounds[0][0], bounds[4][0])
 	var hard_y = bounds[2][1] + 1
 	var random_z = rand_range(bounds[0][2], bounds[1][2])
 	
 	var random_position = Vector3(random_x, hard_y, random_z)
+	gift.gift = rng.randi_range(0, 1) # HARDCODED
 	gift.translation = random_position
 	
 	self.add_child(gift)
--- Objects/Gift/Gift.gd
+++ Objects/Gift/Gift.gd
@@ -1,19 +1,22 @@
 extends KinematicBody
 
 var texture = "res://Art/icon.png"
-var get_gift = 0
+var gift = 0
 
 onready var player = get_parent().get_node("../Player")
 onready var hud = get_parent().get_node("Player").get_node("HUD")
 
-func _ready():
-	$Sprite3D.texture = load(texture)
-	
+func _process(delta):
+	match gift:
+		0:
+			$Sprite3D.texture = load("res://Art/Banana.png")
+		1:
+			$Sprite3D.texture = load("res://Art/Balloons.png")
+
 func _physics_process(delta):
 	$Sprite3D.look_at(Vector3(player.global_transform.origin.x,global_transform.origin.y, player.global_transform.origin.z),Vector3(0,1,0))	
 
 func _on_Area_body_entered(body):
 	if body == player:
-		hud.gift = get_gift
-		print(get_gift)
+		hud.gift = gift
 		queue_free()
