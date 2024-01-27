bcd9886886ee8ac7a8e1de66bb4e270fb39f963a
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 14:58:48 +0200

    Make gift

---
+++ Levels/TestK/TestK.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+onready var timer = $Timer
+onready var hud = $Player/HUD
+
+func make_gift():
+	print("added gift to scene")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.one_shot = false
+	timer.wait_time = 10
+	timer.connect("timeout", self, "_on_timer_timeout")
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	make_gift()
