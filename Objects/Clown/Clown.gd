24621b19b62b27c8f0fdb4455e07bcf52af8e622
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 19:40:24 +0200

    C

--- Objects/Clown/Clown.gd
+++ Objects/Clown/Clown.gd
@@ -19,17 +19,16 @@ func _physics_process(delta):
 		$Sprite3D.look_at(Vector3(player.global_transform.origin.x,global_transform.origin.y, player.global_transform.origin.z),Vector3(0,1,0))
 		var player_position = player.global_transform.origin
 		direction = (player_position - global_transform.origin).normalized()
-	state = ceil((30-funrate)/10)
-	
+	state = ceil((30-funrate) / 10)
 	match state: # texture, speed change
 		0:
-			sprite.texture = load("res://Art/ClownLaugh.png")
+			sprite.set_texture(load("res://Art/ClownNeutral.png"))
 		1:
-			sprite.texture = load("res://Art/ClownNeutral.png")
+			sprite.set_texture(load("res://Art/ClownNeutral.png"))
 		2:
-			sprite.texture = load("res://Art/ClownSadness.png")
+			sprite.set_texture(load("res://Art/ClownNeutral.png"))
 		3:
-			sprite.texture = load("res://Art/ClownAnger.png")
+			sprite.set_texture(load("res://Art/ClownNeutral.png"))
 		
 	move_and_slide(direction * speed)
 	#direction.y = 0
@@ -48,18 +47,19 @@ func _on_Area_area_entered(area):
 			direction = Vector3()
 			yield(get_tree().create_timer(3), "timeout")
 			print("backUp")
+			funrate += 10
 		"Balloon":
 			area.get_parent().activate()
 			stun = true
 			direction = Vector3(0, 1, 0)
 			yield(get_tree().create_timer(2), "timeout")
+			funrate += 10
 		_:
 			drop = false
 			
 	stun = false
-			
+	
 	if drop:
-		funrate += 10
 		area.get_parent().queue_free()
 
 func win():
