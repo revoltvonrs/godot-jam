da8d2724035574deccdf5b190c184ea584a2a8ae
revoltvonrs <154835296+revoltvonrs@users.noreply.github.com>
2024-01-27 14:22:02 +0200

    Revert "Gifts"
    This reverts commit b75678bd8f73243bde64b6c0731889841bb84d13.

--- Objects/Items.gd
+++ Objects/Items.gd
@@ -4,8 +4,7 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-enum Gifts {Banana, Soap}
-
+enum Gifts {gift}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
